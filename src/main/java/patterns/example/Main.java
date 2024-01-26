b7e724b8b82fb7258aa646935982fcc146e0b593
sophxmia <maliarenko.sofiia@gmail.com>
2024-01-26 19:34:02 +0200

    У середовищі Intellij IDEA створити новий проект, що містить один головний клас Мain.

--- src/main/java/patterns/example/Main.java
+++ src/main/java/patterns/example/Main.java
@@ -1,5 +1,42 @@
 package patterns.example;
 
-public class Main {
+/**
+ * @author Sofia Maliarenko
+ *
+ * Task
+ */
 
+public class Main {
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    private void run() {
+        double x = 1.4;
+        double a = 2.7;
+        double b = -0.3;
+        double c = 4.0;
+
+        double result = calculateFunction(x, a, b, c);
+
+        System.out.println("Result: " + result);
+    }
+
+    /**
+     * Counts the function's y-result
+     * @param x x-value
+     * @param a a-value
+     * @param b b-value
+     * @param c c-value
+     * @return the y-value
+     */
+    public double calculateFunction(double x, double a, double b, double c) {
+        if (x < 1.4) {
+            return a * Math.pow(x, 2) + b * x + c;
+        } else if (x == 1.4) {
+            return a / x + Math.sqrt(Math.pow(x, 2) + 1);
+        } else {
+            return (a + b * x) + Math.sqrt(Math.pow(x, 2) + 1);
+        }
+    }
 }
